ffe5d076eac4506a78b2ec89282c242779da50db
Cameron Scott <33464785+CScott96@users.noreply.github.com>
2025-09-08 11:50:04 +0100

    initial board population script

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,22 @@
+const boardCont = document.querySelector("#SketchBoard");
+const titleSection = document.querySelector("#Header");
+
 function initialise() {
-  const titleSection = document.querySelector("#Header");
   const title = document.createElement("h1");
   title.innerText = "Hello world";
   titleSection.appendChild(title);
+  populateBoard();
 }
 initialise();
+
+function populateBoard(totalDivs = 16) {
+  let boardElems = [];
+  for (var i = 0; i < totalDivs; i++) {
+    const div = document.createElement("div");
+    //div.innerText = `I am div no${i}`;
+    boardElems.push(div);
+  }
+  boardElems.forEach((elem) => {
+    boardCont.appendChild(elem);
+  });
+}
